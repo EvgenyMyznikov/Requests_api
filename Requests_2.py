abce4ec7a4f98e15da0102520208d880f4a8340d
EvgenyMyznikov <70178914+EvgenyMyznikov@users.noreply.github.com>
2020-11-06 16:39:53 +0300

    Update Requests_2.py

--- Requests_2.py
+++ Requests_2.py
@@ -1,28 +1,40 @@
 import requests
-from pprint import pprint
-
-token = "..."
 
 
 class YaUploader:
-    def __init__(self, token: "..."):
-        self.token = token
+    def __init__(self, auth_token):
+        self.token = auth_token
+        self.headers = {"Authorization": f"OAuth {token}"}
+        
 
-    def get_url_disk(self, url="https://cloud-api.yandex.net/v1/disk/resources/upload"):
-        self.url = url
-        HEADERS = {"Authorization": f"OAuth {token}"}
-        response = requests.get(url, params={"path": 'https://yadi.sk/d/GdxAH0dkrbmOaA?w=1'}, headers=HEADERS)
-        return response.json()['href']
+    def get_upload_url(self, file_name):
+        URL="https://cloud-api.yandex.net/v1/disk/resources/upload"
+        
+        PARAMS = {'path': '/Photos/' + file_name, 'overwrite': True}
+        
+        upload_url = requests.get(url=URL, params=PARAMS, headers=self.headers)
+        if upload_url.status_code == 200:
+            return upload_url.json()['href']
+        else:
+            print('Error!')
 
-    def upload(self, file_path: "/Users/evgenym/Documents/NETOLOGY-DIPLOM/Hello world!.txt"):
-        self.file_path = file_path
-        with open('Hello world!.txt', 'rb') as f:
-            response = requests.put('https://uploader7j.disk.yandex.net:443/upload-target/20201027T123137.939.utd.dzi57s2nkad8i8f907ms8bsug-k7j.21048142', files={"file": f})
-        print(response.status_code)
+    def upload(self, url, file_path):
+        print(file_path)
+        with open(file_path, 'rb') as f:
+            response = requests.put(url, data=f)
+            if response.status_code == 201:
+                print('file uploaded successfully!')
 
 
 if __name__ == '__main__':
-    uploader = YaUploader('...')
-    result = uploader.upload('/Users/evgenym/Documents/NETOLOGY-DIPLOM/Hello world!.txt')
+    
+    token = "..."
+    
+    path = 'Hello world!.txt'
+    file = path
+    
+    uploader = YaUploader(token)
 
-pprint(YaUploader(token))
+    upload_url = uploader.get_upload_url(file)
+    result = uploader.upload(upload_url, path)
+    
